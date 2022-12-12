610d1a308bc5787043ae4d66974fb97af295cd35
jskaza <jonathan.skaza@gmail.com>
2022-12-11 21:19:08 -0500

    2022 day 11 puzzle1

---
+++ 2022/12.11.22/puzzle1.nim
@@ -0,0 +1,73 @@
+import strutils
+# neat little mathematical expression evaluator library 
+# https://yardanico.github.io/nim-mathexpr/mathexpr.html
+import mathexpr
+import math
+import algorithm
+
+let input = readFile("input.txt")
+
+type
+  Monkey = object
+    items: seq[string]
+    operation: string
+    test: float
+    ifTrue: int
+    ifFalse: int
+
+var 
+    monkeys: seq[Monkey]
+
+const rounds = 20
+
+proc parseItems(s: string): seq[string] =
+    let itemsStr = s.split(": ")[1]
+    return itemsStr.split(", ")
+
+proc parseOp(s: string): string =
+    return s.split("= ")[1]
+
+proc parseTest(s: string): float =
+    return parseFloat(s.split("by ")[1])
+
+proc parseThrow(s: string): int =
+    return parseInt($s[^1])
+
+proc performOp(op, old: string): float =
+    let ev = newEvaluator()
+    return floor(ev.eval(op.replace("old", old))/3)
+
+
+for line in splitLines(input):
+    if line.startsWith("M"):
+        monkeys.add(Monkey())
+    elif line.contains("Starting"):
+        monkeys[^1].items = parseItems(line)
+    elif line.contains("Operation"):
+        monkeys[^1].operation = parseOp(line)
+    elif line.contains("Test"):
+        monkeys[^1].test = parseTest(line)
+    elif line.contains("true"):
+        monkeys[^1].ifTrue = parseThrow(line)
+    elif line.contains("false"):
+         monkeys[^1].ifFalse = parseThrow(line)
+    else:
+        continue
+
+var inspections = newSeq[int](len(monkeys))
+
+for i in 1..rounds:
+    for idx, monkey in monkeys:
+        for item in monkey.items:
+            inspections[idx] += 1
+            let worryLevel = performOp(monkey.operation, item)
+            if (worryLevel mod monkey.test) == 0:
+                monkeys[monkey.ifTrue].items.add($worryLevel)
+            else:
+                monkeys[monkey.ifFalse].items.add($worryLevel)
+        
+        monkeys[idx].items = @[] # important to reset after each round
+
+inspections.sort()
+
+echo inspections[^1] * inspections[^2]
